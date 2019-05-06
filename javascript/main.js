c5d06c486739daaba2dd833a5a4b4fe9bdb4919e
censuradho <gustavoleiteoliveira800@gmail.com>
2019-05-06 17:51:50 -0300

    i modified the index.html and main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 i = 0 
 function change_image() {
-    document.getElementById('slideShow').src = '_imagens/slideShow' + (i++ % 3)+ '.jpg';
+    document.getElementById('slideShow').src = 'imagens/slideShow' + (i++ % 3)+ '.jpg';
 }
 function slider_timer() {
     setInterval(change_image, 4*1000)
