7c15a752bc07120ca1effd64f3d0bcc4ca5b3e32
censuradho <gustavoleiteoliveira800@gmail.com>
2019-05-06 17:32:13 -0300

    firist commit

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+i = 0 
+function change_image() {
+    document.getElementById('slideShow').src = '_imagens/slideShow' + (i++ % 3)+ '.jpg';
+}
+function slider_timer() {
+    setInterval(change_image, 4*1000)
+}
+window.addEventListener('load', slider)
