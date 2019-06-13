3fbd7d3eb29bb1ca12235c626c6d6502921c51af
censuradho <gustavoleiteoliveira800@gmail.com>
2019-06-13 13:45:01 -0300

    transição suave entre os links

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,30 @@ function slider_timer() {
     setInterval(change_image, 4*1000)
 }
 window.addEventListener('load', slider_timer)
+
+// Scroll suave
+const menuItens = document.querySelectorAll('#home a[href^="#"]')
+console.log(menuItens)
+menuItens.forEach(itens=> {
+    itens.addEventListener('click', scrollToIdOnClick)
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getElementByHref(event.target);
+    scrollToPosition(to)
+ 
+    function scrollToPosition() {
+        window.scroll({
+            top: to,
+            behavior: 'smooth',
+        })
+    }
+    
+    function getElementByHref(target) {
+        const id = target.getAttribute('href')
+        return document.querySelector(id).offsetTop
+    }
+
+}
+
